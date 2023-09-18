70bc6cd6892787dc9a14467fce685db8fdee8038
MarcLipe <marcos.lima52@fatec.sp.gov.br>
2023-09-18 18:32:16 -0300

    Adding Route Start

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { DefaultLayout } from "./layouts/DefaultLayout";
+import { Start } from "./pages/Start";
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout/>}>
         <Route path="/" element={<Home/>} />
+        <Route path="/start" element={<Start/>}/>
       </Route>
     </Routes>
   )
--- src/pages/Start/index.tsx
+++ src/pages/Start/index.tsx
@@ -1,7 +1,19 @@
+import imgCart from '../../assets/images/shopping-kart.jpg'
+
 export function Start() {
   return (
-    <div>
+    <main>
       <h1>Start</h1>
-    </div>
+      <section>
+        <div>
+          <h1>GroList</h1>
+          <img src="" alt="" />
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+      </section>
+      <section>
+        <img src={imgCart} alt="" />
+      </section>
+    </main>
   )
 }
