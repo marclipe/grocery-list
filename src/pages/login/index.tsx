a5eb7a46fa081e5e36d6231a95f1f90dde406bc1
MarcLipe <marcos.lima52@fatec.sp.gov.br>
2023-09-20 11:50:55 -0300

    Adding Zod and React-Hook-Form for validation screen Login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,11 +2,31 @@ import { useState } from 'react'
 import './styles.scss'
 import imgCart from '../../assets/images/grocery-image.jpg'
 import { Link } from 'react-router-dom'
-import { Auth } from '../auth'
+import { z } from 'zod'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const formShchema = z.object({
+  email: z.string().email("Email inválido").min(1, "O email é obrigatório"),
+  password: z
+    .string()
+    .min(1, "A senha é obrigatória!")
+    .min(8, "A senha deve ter mais de 8 caracteres"),
+});
 
 export function Login() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+
+  type FormShemaType = z.infer<typeof formShchema>;
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<FormShemaType>({
+    resolver: zodResolver(formShchema),
+  });
 
   const HandleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
@@ -23,6 +43,7 @@ export function Login() {
               type="email"
               value={email}
               id="iemail"
+              placeholder="email"
               onChange={(e) => setEmail(e.target.value)}
               required
             />
@@ -33,13 +54,24 @@ export function Login() {
               type="password"
               value={password}
               id="ipassword"
+              placeholder="••••••••"
               onChange={(e) => setPassword(e.target.value)}
               required
             />
           </div>
-          <Link  to="/auth">Não possui conta?</Link>
+          <div className="forgot-password">
+            <Link to="/">
+              <p>Esqueci senha</p>
+            </Link>
+          </div>
           <div>
             <button type="submit">Entrar</button>
+          </div>
+          <div className="link-auth">
+            Não possui conta?
+            <Link to="/auth">
+              <strong>Criar conta</strong>
+            </Link>
           </div>
         </form>
       </section>
