e123d85e53da027f74992b9881f978280dfc5434
MarcLipe <marcos.lima52@fatec.sp.gov.br>
2023-09-20 19:51:15 -0300

    Adding the page Not Found

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,15 @@
+import './styles.scss'
+
+export function NotFound() {
+ return (
+   <div className="notfound-container">
+     <div className="notfound-content">
+       <div>
+         <h3>Página não encontrada :(</h3>
+         <h1>OOPS!!</h1>
+         <p>A página que você está procurando não existe.</p>
+       </div>
+     </div>
+   </div>
+ ); 
+}
