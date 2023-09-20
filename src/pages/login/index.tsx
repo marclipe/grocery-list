45e7ff95d1c9b1323fb482e08817a76f88ac7fa1
MarcLipe <marcos.lima52@fatec.sp.gov.br>
2023-09-20 16:13:13 -0300

    Page of Login is done!

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,63 +1,77 @@
-import { useState } from 'react'
 import './styles.scss'
 import imgCart from '../../assets/images/grocery-image.jpg'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { z } from 'zod'
 import { SubmitHandler, useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 
-const formShchema = z.object({
-  email: z.string().email("Email inválido").min(1, "O email é obrigatório"),
-  password: z
-    .string()
-    .min(1, "A senha é obrigatória!")
-    .min(8, "A senha deve ter mais de 8 caracteres"),
-});
-
 export function Login() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const formSchema = z.object({
+    email: z.string().email("Email inválido").min(1, "O email é obrigatório"),
+    password: z
+      .string()
+      .min(1, "A senha é obrigatória!")
+      .min(8, "A senha deve ter mais de 8 caracteres"),
+  });   
 
-  type FormShemaType = z.infer<typeof formShchema>;
+  type FormSchemaType = z.infer<typeof formSchema>;
 
   const {
     register,
     handleSubmit,
     formState: { errors, isSubmitting },
-  } = useForm<FormShemaType>({
-    resolver: zodResolver(formShchema),
+  } = useForm<FormSchemaType>({
+    resolver: zodResolver(formSchema),
   });
 
-  const HandleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-  }
+  const navigate = useNavigate();
+
+  const onSubmit: SubmitHandler<FormSchemaType> = async (data) => {
+    try {
+      // await enviar dados para backend
+      console.log(data);
+      navigate('/')
+    } catch(error) {
+      console.error('Erro ao enviar formulário!', error)
+    }
+  };
 
   return (
     <main className="section-container">
       <section className="form-section">
-        <form /*autoComplete='off'*/ onSubmit={HandleSubmit}>
+        <form /*autoComplete='off'*/ onSubmit={handleSubmit(onSubmit)}>
           <h2>Login</h2>
           <div>
             <label htmlFor="iemail">Email: </label>
             <input
               type="email"
-              value={email}
               id="iemail"
               placeholder="email"
-              onChange={(e) => setEmail(e.target.value)}
-              required
+              {...register("email")}
             />
+            {
+              errors.email && (
+                <span className='errors-message'>
+                  {errors.email?.message}
+                </span>    
+              )
+            }
           </div>
           <div>
             <label htmlFor="ipassword">Senha: </label>
             <input
               type="password"
-              value={password}
               id="ipassword"
               placeholder="••••••••"
-              onChange={(e) => setPassword(e.target.value)}
-              required
+              {...register("password")}
             />
+            {
+              errors.password && (
+                <span className="errors-message">
+                  {errors.password?.message}
+                </span>
+              )
+            }
           </div>
           <div className="forgot-password">
             <Link to="/">
@@ -65,7 +79,9 @@ export function Login() {
             </Link>
           </div>
           <div>
-            <button type="submit">Entrar</button>
+            <button type="submit" disabled={isSubmitting}>
+              Entrar
+            </button>
           </div>
           <div className="link-auth">
             Não possui conta?
