9e10bcb51b85257e6fe5f4cc0c90bab512c63573
MarcLipe <marcos.lima52@fatec.sp.gov.br>
2023-09-20 19:51:15 -0300

    Adding the page Not Found

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,7 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { DefaultLayout } from "./layouts/DefaultLayout";
 import { Start } from "./pages/Start";
-import { Login } from "./pages/login";
+import { Login } from "./pages/Login";
+import { NotFound } from "./pages/NotFound";
 
 export function Router() {
   return (
@@ -11,6 +12,7 @@ export function Router() {
         <Route path="/" element={<Home/>} />
         <Route path="/start" element={<Start/>}/>
         <Route path="/login" element={<Login/>}/>
+        <Route path="*" element={<NotFound/>}/>
       </Route>
     </Routes>
   )
