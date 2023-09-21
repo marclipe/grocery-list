381f828a2d6a4a4f65f60a67e25746669ac3fa31
MarcLipe <marcos.lima52@fatec.sp.gov.br>
2023-09-20 22:00:26 -0300

    Page of auth to Create user

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,6 +4,7 @@ import { DefaultLayout } from "./layouts/DefaultLayout";
 import { Start } from "./pages/Start";
 import { Login } from "./pages/Login";
 import { NotFound } from "./pages/NotFound";
+import { Auth } from "./pages/auth";
 
 export function Router() {
   return (
@@ -12,6 +13,7 @@ export function Router() {
         <Route path="/" element={<Home/>} />
         <Route path="/start" element={<Start/>}/>
         <Route path="/login" element={<Login/>}/>
+        <Route path="/auth" element={<Auth/>}/>
         <Route path="*" element={<NotFound/>}/>
       </Route>
     </Routes>
--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,5 +1,51 @@
+import imgGroceryAuth from '../../assets/images/grrocey-auth.jpg'
+import './styles.scss'
+
 export function Auth() {
   return (
-    <h1>Auth</h1>
-  )
+    <main className="container-auth">
+      <section className="content-section">
+        <div className="container-form">
+          <form action="">
+            <div>
+              <label htmlFor="iusername">Seu Nome</label>
+              <input type="text" id="iusername" placeholder="name" />
+            </div>
+            <div>
+              <label htmlFor="iemail">Seu Email</label>
+              <input type="email" id="iemail" placeholder="name@email.com" />
+            </div>
+            <div>
+              <label htmlFor="ipassword">Senha</label>
+              <input type="password" id="ipassword" placeholder="••••••••" />
+            </div>
+            <div>
+              <label htmlFor="iconfirm-password">Confirmar Senha</label>
+              <input
+                type="password"
+                id="confirm-password"
+                placeholder="••••••••"
+              />
+            </div>
+            <div className="container-terms">
+              <div>
+                <input type="checkbox" id="iterms" />
+              </div>
+              <div>
+                <label htmlFor="iterms">
+                  I accept the <a href="#">Terms and Conditions</a>
+                </label>
+              </div>
+            </div>
+            <div className='button-container'>
+              <button type="submit">Criar conta</button>
+            </div>
+          </form>
+        </div>
+      </section>
+      <section className="image-section">
+        <img src={imgGroceryAuth} alt="" />
+      </section>
+    </main>
+  );
 }
